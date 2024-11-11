f7170032e710fe364d7f5ac371bc2c49a5269008
Sara Ajala <sara.ajalasiva24@gmail.com>
2024-11-11 15:42:08 -0300

    finalizado (sem css)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once 'C:/aluno2/xampp/htdocs/receitasculinarias/config.php';
 require_once 'C:/aluno2/xampp/htdocs/receitasculinarias/controller/ReceitaController.php';
 
 $receitaController = new ReceitaController($pdo);
-$receitaController->exibirListaReceitas();
+/*$receitaController->exibirListaReceitas();*/
 
 
 ?>
