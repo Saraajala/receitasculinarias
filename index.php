50dc2b7201a6c76497c050776bc3b380c604a452
Raissa <raissacruz.sesisenai@gmailcom>
2024-11-14 09:15:48 -0300

    inicio do css

--- index.php
+++ index.php
@@ -12,13 +12,40 @@ $receitaController = new ReceitaController($pdo);
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+
+<header class="fundoindex">
+    <h1>Aroma & Sabor</h1>
+    <div class="menu">
+        <ul>
+            <li><a href="../../cadastrar.php">Cadastre-se</a></li>
+            <li><a href="../../login.php">Login</a></li>
+            <li><a href="blog.php">Blog</a></li>
+        </ul>
+    </div>
+    <h3>Olá e seja bem-vindo! No nosso blog de culinária, você encontra inspirações, receitas e dicas para tornar
+        cada refeição especial. Explore e deixe-se levar pelo sabor!
+    </h3>
+    <div class="btn">
+        <a href="sobre.php"><button>Saiba mais</button></a>
+    </div>
+</header>
+
+
+    <footer class="rodape2">
+            <div>
+            <p>© 2024 Sara Ajala e Raissa Cruz. Todos os direitos reservados</p>
+            </div>
+        </footer>
 </body>
+
 </html>
