4a08b4e5e9c7801cb4df21bba5dc6e6b3f4afbbb
Sara Ajala <sara.ajalasiva24@gmail.com>
2024-11-14 11:35:35 -0300

    completando com css as paginas (sobre, cadastrar e login)

--- index.php
+++ index.php
@@ -23,29 +23,33 @@ $receitaController = new ReceitaController($pdo);
 <body>
 
 
-<header class="fundoindex">
-    <h1>Aroma & Sabor</h1>
-    <div class="menu">
-        <ul>
-            <li><a href="../../cadastrar.php">Cadastre-se</a></li>
-            <li><a href="../../login.php">Login</a></li>
-            <li><a href="blog.php">Blog</a></li>
-        </ul>
-    </div>
-    <h3>Olá e seja bem-vindo! No nosso blog de culinária, você encontra inspirações, receitas e dicas para tornar
-        cada refeição especial. Explore e deixe-se levar pelo sabor!
-    </h3>
-    <div class="btn">
-        <a href="sobre.php"><button>Saiba mais</button></a>
-    </div>
-</header>
+    <header class="fundoindex">
+
+        <div class="menu">
+            <h1><a href="index.php">Aroma & Sabor</a></h1>
+            <ul>
+                <li><a href="view/usuario/cadastrar.php">Cadastre-se</a></li>
+                <li><a href="view/usuario/login.php">Login</a></li>
+                <li><a href="blog.php">Blog</a></li>
+            </ul>
+        </div>
+
+        <br><br><br><br><br><br>
+
+        <h3>Olá e seja bem-vindo! <br> No nosso blog de culinária, você encontra inspirações, receitas e dicas para tornar
+            cada refeição especial. Explore e deixe-se levar pelo sabor!
+        </h3>
+        <div class="btn">
+            <a href="sobre.php"><button>Saiba mais</button></a>
+        </div>
+    </header>
 
 
     <footer class="rodape2">
-            <div>
+        <div>
             <p>© 2024 Sara Ajala e Raissa Cruz. Todos os direitos reservados</p>
-            </div>
-        </footer>
+        </div>
+    </footer>
 </body>
 
 </html>
