4762e845cc8047806253ea32a55d7cf3f478d610
Sara Ajala <sara.ajalasiva24@gmail.com>
2024-11-14 09:15:23 -0300

    mudanças

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $receitaController = new ReceitaController($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Aroma & Sabor</title>
 </head>
 <body>
     
