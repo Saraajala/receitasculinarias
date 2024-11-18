2a68ddd9eb5c7f9451cd4c7867ba50a726e36262
Raissa <raissacruz.sesisenai@gmailcom>
2024-11-18 11:25:24 -0300

    imagem e descrição da categoria bolo

--- categorias/bolos.php
+++ categorias/bolos.php
@@ -28,63 +28,71 @@
         <div class="lista-receitas">
             
             <article class="receita">
-                <h3>Título da Receita 1</h3>
-                <p>Descrição breve da receita 1...</p>
-                <a href="receita1.php" class="ver-mais">Ver mais</a>
+                <h3> Bolo Red Velvet</h3>
+                <img src="/img/imgbolo/bolo red velvet.jpg">
+                <p>Macio e úmido, com massa vermelho-aveludada e cobertura cremosa de cream cheese.</p>
+                <a href="receitaescritas/boloredvelvet.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 2</h3>
-                <p>Descrição breve da receita 2...</p>
-                <a href="receita2.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de cenoura</h3>
+                <img src="img/imgbolo/bolo de cenoura.jpg">
+                <p>Fofinho e dourado, com sabor suave de cenoura e uma irresistível cobertura de chocolate.</p>
+                <a href="receitasescritas/bolodecenoura.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 3</h3>
-                <p>Descrição breve da receita 3...</p>
-                <a href="receita3.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de chocolate</h3>
+                <img src="img/imgbolo/bolochocolate.webp">
+                <p>Rico, úmido e cheio de sabor, coberto com uma camada cremosa de ganache ou calda de chocolate.</p>
+                <a href="receitasescritas/bolochocolate.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 4</h3>
-                <p>Descrição breve da receita 4...</p>
-                <a href="receita3.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo floresta negra</h3>
+                <img src="img/imgbolo/bolo-floresta-negra.jpg">
+                <p>Combinação irresistível de chocolate, chantilly e cerejas, em camadas perfeitas.</p>
+                <a href="receitasescritas/boloflorestanegra.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 5</h3>
-                <p>Descrição breve da receita 5...</p>
-                <a href="receita1.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de pistache</h3>
+                <img src="img/imgbolo/bolo de pistache.jpg">
+                <p>Delicado e aromático, com sabor suave de pistache e cobertura cremosa.</p>
+                <a href="receitasescritas/bolopistache.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 6</h3>
-                <p>Descrição breve da receita 6...</p>
-                <a href="receita2.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de fubá</h3>
+                <img src="img/imgbolo/bolo de fubá.jpg">
+                <p>Simples e macio, com sabor caseiro e toque levemente adocicado de milho.</p>
+                <a href="receitasescritas/bolodefuba.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 7</h3>
-                <p>Descrição breve da receita 7...</p>
-                <a href="receita3.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de milho</h3>
+                <img src="img/imgbolo/bolo de milho.jpg">
+                <p>Fofinho e saboroso, com o autêntico gosto do milho fresco.</p>
+                <a href="receitasescritas/bolodemilho.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 8</h3>
-                <p>Descrição breve da receita 8...</p>
-                <a href="receita3.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de prestígio</h3>
+                <img src="img/imgbolo/bolo de prestigio.webp">
+                <p>Combinação perfeita de chocolate e coco, com camadas cremosas e sabor irresistível.</p>
+                <a href="receitasescritas/bolodeprestigio.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 9</h3>
-                <p>Descrição breve da receita 9...</p>
-                <a href="receita3.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo mesclado</h3>
+                <p>Mistura macia de massas de baunilha e chocolate, formando um visual e sabor irresistíveis.</p>
+                <a href="receitaescritas/bolomesclado.php" class="ver-mais">Ver mais</a>
             </article>
 
             <article class="receita">
-                <h3>Título da Receita 10</h3>
-                <p>Descrição breve da receita 10...</p>
-                <a href="receita3.php" class="ver-mais">Ver mais</a>
+                <h3>Bolo de baunilha</h3>
+                <p> Leve, macio e aromático, com o sabor clássico e delicado da baunilha.</p>
+                <a href="receitasescritas/bolodebaunilha.php" class="ver-mais">Ver mais</a>
             </article>
 
         </div>
