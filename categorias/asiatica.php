4aeba9eeb53a770e214aa9f119289047e0279e88
Sara Ajala <sara.ajalasiva24@gmail.com>
2024-11-18 15:42:55 -0300

    termino da página doces de natal

---
+++ categorias/asiatica.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>culinária asiática</title>
+</head>
+<body>
+<header class="blog">
+        <div class="menu">
+            <h1><a href="index.php">Aroma & Sabor</a></h1>
+            <ul>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="index.php">Sair</a></li>
+            </ul>
+        </div>
+        <section class="container2">
+        <div class="btn">
+            <a href="../view/receita/cadastrar2.php"><button>Cadastrar Nova Receita +</button></a>
+        </div>
+        </section>
+    </header>
+
+    <!-- Seção de Receitas de culinária asiática -->
+    <section class="receitas-recentes">
+        <h2>Culinária Asiática</h2>
+        <div class="lista-receitas">
+            
+            <article class="receita">
+                <h3>Sushi</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/sushi.jpg">
+                <p>Sushi é arroz temperado com vinagre, servido com peixe cru ou outros ingredientes.</p>
+                <a href="receitaescritas/sushi.php "class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Pho</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/pho.avif">
+                <p>Pho é uma sopa vietnamita feita com caldo quente, macarrão de arroz, ervas frescas e carne, geralmente de boi ou frango.</p>
+                <a href="receitasescritas/pho.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Dim sum</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/dimsum.jpg">
+                <p>Dimsum são pequenos pratos chineses cozidos no vapor com recheios diversos.</p>
+                <a href="receitasescritas/dimsum.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Pad Thai</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/pad thai.jpg">
+                <p>Pad Thai é um macarrão tailandês frito com camarões, tofu e molho agridoce.</p>
+                <a href="receitasescritas/padthai.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Ramen</h3>
+                <img class="img-receita2" src="../img/img culinaria asiatica/ramen.jpg">
+                <p>Ramen é uma sopa japonesa de macarrão em caldo quente, com carne, ovos e vegetais.</p>
+                <a href="receitasescritas/ramen.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Banh Mi</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/banh mi.webp">
+                <p>Banh Mi é um sanduíche vietnamita feito com baguete, carne, vegetais e molho.</p>
+                <a href="receitasescritas/banhmi.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Bibimbap</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/bibimbap.jpg">
+                <p>Bibimbap é um prato coreano de arroz servido com legumes, carne, ovo e molho picante.</p>
+                <a href="receitasescritas/bibimbap.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Curry tailândes</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/curry tailandes.png">
+                <p>Curry tailandês é um prato de pasta de curry, leite de coco e carne ou legumes.</p>
+                <a href="receitasescritas/currytailandes.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Satay</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/satay.jpg">
+                <p>Satay é um prato indonésio de espetinhos de carne grelhada, servidos com molho de amendoim.</p>
+                <a href="receitaescritas/satay.php" class="ver-mais">Ver mais</a>
+            </article>
+
+            <article class="receita">
+                <h3>Tonkatsu</h3>
+                <img class="img-receita" src="../img/img culinaria asiatica/tonkatsu.jpg">
+                <p>Tonkatsu é um prato japonês de carne de porco empanada e frita, servida com molho tonkatsu.</p>
+                <a href="receitaescritas/tonkatsu.php" class="ver-mais">Ver mais</a>
+            </article>
+
+     
+
+        </div>
+    </section>
+
+    <br><br><br><br>
+
+    <footer class="rodape2">
+        <div>
+            <p>© 2024 Sara Ajala e Raissa Cruz. Todos os direitos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
